cda3b21c052a3b1eb0656e2a8a3bb6fd399ee075
Jason Kannemeyer <vonhitl@gmail.com>
2023-07-26 12:29:16 -0700

    updated files

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,19 @@
 #include "main.h"
 
 /**
- * main - print each argument passed to program on a line
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ *main - print each argument passed to program on a line
+ *@argc: argument counter
+ *@argv: pointer to array of arguments
+ *Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-		int i = 0;
+	int i = 0;
 
-			while (i < argc)
-						printf("%s\n", argv[i++]);
+	while (i < argc)
 
-				return (0);
+		printf("%s\n", argv[i++]);
+
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,12 @@
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* atoi */
-#include <stdbool.h> /* bool data type */
+#include <stdio.h > /*printf */
+#include <stdlib.h > /*atoi */
+#include <stdbool.h > /*bool data type */
 
 /**
- * is_num - iterate through each argv to test if it's a number
- * @argvv: a argv
- * Return: true only if entire string is a number, false if not
- */
+ *  *is_num - iterate through each argv to test if it's a number
+ *   *@argvv: a argv
+ *    *Return: true only if entire string is a number, false if not
+ *     */
 
 bool is_num(char *argvv)
 {
@@ -17,29 +17,30 @@ bool is_num(char *argvv)
 								if (!(argvv[j] >= '0' && argvv[j] <= '9'))
 												return (0);
 									}
+
 				return (1);
 }
 
 /**
- * main - print sum if all arguments given are numbers
- * @argc: argument counter
- * @argv: arguments
- * Return: 0 on success, 1 if an argument wasn't a number
- */
+ *  *main - print sum if all arguments given are numbers
+ *   *@argc: argument counter
+ *    *@argv: arguments
+ *     *Return: 0 on success, 1 if an argument wasn't a number
+ *      */
 
 int main(int argc, char *argv[])
 {
 		int i = 1;
 			int sum = 0;
 
-				/* validate input */
+				/*validate input */
 				if (argc == 1)
 						{
 									printf("0\n");
 											return (0);
 												}
 
-					/* check all arguments to add numbers */
+					/*check all arguments to add numbers */
 					while (i < argc)
 							{
 										if (is_num(argv[i]))
@@ -49,8 +50,10 @@ int main(int argc, char *argv[])
 																			printf("Error\n");
 																						return (1);
 																								}
+
 														i++;
 															}
+
 						printf("%d\n", sum);
 
 							return (0);
